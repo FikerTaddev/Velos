cf441d1084a4ea7bd171c8a80bd3b2e66307a2da
fikertaddev <fikertad.dev@hotmail.com>
2026-03-09 12:49:30 +0300

    fix(H) : fixed even handler issue

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { H } from "./Runtime/H.js"
-import { render } from "./Runtime/render.js"
-
-const app = H(
-  "div",
-  null,
-  "<h1>Hello , Velos</h1>"
-)
-
-render(app, document.body)
+export * from "./runtime/H.js"
+export * from "./runtime/render.js"
+export * from "./reactivity/state.js"
